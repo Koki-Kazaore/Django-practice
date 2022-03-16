35cc9078b013066f66c3ce5e3de66f0e004aab80
ka300-g <kazaore@icloud.com>
2022-03-16 18:27:11 +0900

    completed Django form section

--- cs50-django/PROJECT_NAME/tasks/views.py
+++ cs50-django/PROJECT_NAME/tasks/views.py
@@ -1,6 +1,14 @@
+from audioop import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -10,4 +18,20 @@ def index(request):
 
 # Add a new task:
 def add(request):
-    return render(request, "tasks/add.html")
+
+  if request.method == 'POST':
+    form = NewTaskForm(request.POST)
+
+    if form.is_valid():
+      task = form.cleaned_data['task']
+      tasks.append(task)
+      return HttpResponseRedirect(reverse('tasks:index'))
+
+    else:
+      return render(request, 'tasks:add.html', {
+        'form': form
+      })
+
+  return render(request, "tasks/add.html", {
+    "form": NewTaskForm()
+  })
